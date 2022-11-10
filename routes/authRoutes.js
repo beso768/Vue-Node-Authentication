a68f87858fe1277bf7e85e27d59a3f8d87027a2a
beso <besogigashvili818@gmail.com>
2022-11-10 19:59:35 +0400

    made changes into route links

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,10 +5,14 @@ const isAuthenticated = require("../middleware/isAuthenticated");
 
 const router = Router();
 
-router.post("/auth/sign-up", authController.signup);
-router.post("/auth/sign-in", authController.signin);
-router.delete("/users/:userId", isAuthenticated, userhController.delete_user);
-router.get("/auth/me", isAuthenticated, userhController.whoami);
-router.put("/users/:userId", isAuthenticated, userhController.update_user);
+router.post("/api/auth/sign-up", authController.signup);
+router.post("/api/auth/sign-in", authController.signin);
+router.delete(
+  "/api/users/:userId",
+  isAuthenticated,
+  userhController.delete_user
+);
+router.get("/api/auth/me", isAuthenticated, userhController.whoami);
+router.put("/api/users/:userId", isAuthenticated, userhController.update_user);
 
 module.exports = router;
